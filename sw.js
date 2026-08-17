6a61d3f23d526e89f7885e502b0ea650a0786610
Hanataro-hanahana <koutarou.0106@gmail.com>
2026-08-17 11:47:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='music-theory-embedded-v6';
-self.addEventListener('install',e=>self.skipWaiting());
+const CACHE='music-theory-pwa7';
+const ASSETS=['./','./index.html','./data.json','./manifest.webmanifest','./icon-180.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
